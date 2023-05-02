04d888f9abcf6e2fbcf7f0e1ece9311333940430
Francis Matias <fwmatias64@gmail.com>
2023-05-02 23:06:04 +0100

    Update word.js

--- app/main.js
+++ app/main.js
@@ -3,81 +3,18 @@ const typingInput = document.querySelector(".typing-input")
 const hint = document.querySelector(".hint span")
 wrongLetter = document.querySelector(".wrong-letter span")
 guessLeft = document.querySelector(".guess-left span")
-resetBtn = document.querySelector(".reset-btn")
-let word, maxGuesses = [] , corrects = [], incorrects = [];
-
-let wordList = [
-
-  {
-    word: "apple",
-    hint: "A popular fruit that is often associated with teachers."
-  },
-  {
-    word: "banana",
-    hint: "A yellow fruit that is a great source of potassium."
-  },
-  {
-    word: "cherry",
-    hint: "A small red fruit that is often used to make pies."
-  },
-  {
-    word: "grape",
-    hint: "A small, round fruit that is often used to make wine."
-  },
-  {
-    word: "kiwi",
-    hint: "A small, brown fruit that is known for its fuzzy skin and bright green flesh."
-  },
-  {
-    word: "lemon",
-    hint: "A sour fruit that is often used to make lemonade."
-  },
-  {
-    word: "mango",
-    hint: "A sweet fruit that is popular in tropical regions."
-  },
-  {
-    word: "orange",
-    hint: "A citrus fruit that is commonly consumed as a juice or eaten as a snack."
-  },
-  {
-    word: "papaya",
-    hint: "A large fruit with orange flesh and black seeds that is often eaten for breakfast."
-  },
-  {
-    word: "pineapple",
-    hint: "A tropical fruit that has a tough outer layer and sweet, juicy flesh inside."
-  },
-  {
-    word: "raspberry",
-    hint: "A small, red fruit that grows on bushes and is often used to make jam."
-  },
-  {
-    word: "strawberry",
-    hint: "A small, red fruit that is often used to make desserts."
-  },
-  {
-    word: "watermelon",
-    hint: "A large, juicy fruit with a green outer layer and pink flesh inside."
-  },
-  {
-    word: "grapefruit",
-    hint: "A large, citrus fruit that is often eaten for breakfast."
-  },
-  {
-    word: "blueberry",
-    hint: "A small, blue fruit that is often used in baking and is high in antioxidants."
-  }
-]
-
-
+resetBtn =  document.querySelector(".reset-btn")
+let word, 
+maxGuesses = [],
+corrects = [],
+incorrects = [];
 
 
 function randomWord() {
   //get random obj from the list
   let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
   word = ranObj.word;//get a random WORD from obj
-  maxGuesses = 4; 
+  maxGuesses = 6; 
   corrects = [];
   incorrects = []; //reser all values to default
 
@@ -102,6 +39,8 @@ function initGame(e) {
       for (let i = 0; i < word.length; i++) {
         if (word[i] === key) {
           inputs.querySelectorAll("input")[i].value = key;
+          corrects.push(key)
+          console.log(corrects)
         }
       }
     } else {
@@ -124,7 +63,6 @@ function initGame(e) {
       }
     }
     })
-
 } 
 
 
---
+++ app/word.js
@@ -0,0 +1,64 @@
+let wordList = [
+
+    {
+      word: "apple",
+      hint: "A popular fruit that is often associated with teachers."
+    },
+    {
+      word: "banana",
+      hint: "A yellow fruit that is a great source of potassium."
+    },
+    {
+      word: "cherry",
+      hint: "A small red fruit that is often used to make pies."
+    },
+    {
+      word: "grape",
+      hint: "A small, round fruit that is often used to make wine."
+    },
+    {
+      word: "kiwi",
+      hint: "A small, brown fruit that is known for its fuzzy skin and bright green flesh."
+    },
+    {
+      word: "lemon",
+      hint: "A sour fruit that is often used to make lemonade."
+    },
+    {
+      word: "mango",
+      hint: "A sweet fruit that is popular in tropical regions."
+    },
+    {
+      word: "orange",
+      hint: "A citrus fruit that is commonly consumed as a juice or eaten as a snack."
+    },
+    {
+      word: "papaya",
+      hint: "A large fruit with orange flesh and black seeds that is often eaten for breakfast."
+    },
+    {
+      word: "pineapple",
+      hint: "A tropical fruit that has a tough outer layer and sweet, juicy flesh inside."
+    },
+    {
+      word: "raspberry",
+      hint: "A small, red fruit that grows on bushes and is often used to make jam."
+    },
+    {
+      word: "strawberry",
+      hint: "A small, red fruit that is often used to make desserts."
+    },
+    {
+      word: "watermelon",
+      hint: "A large, juicy fruit with a green outer layer and pink flesh inside."
+    },
+    {
+      word: "grapefruit",
+      hint: "A large, citrus fruit that is often eaten for breakfast."
+    },
+    {
+      word: "blueberry",
+      hint: "A small, blue fruit that is often used in baking and is high in antioxidants."
+    }
+  ]
+  
